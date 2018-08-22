4c141e3415d1d14d9f0e8f249aa91cf6b98d7598
EdsonRui <edsonrui@outlook.com>
2018-08-22 14:50:53 -0400

    REST testado

---
+++ src/main/java/com/nelionovais/cursomc/recources/CategoriaRecource.java
@@ -0,0 +1,15 @@
+package com.nelionovais.cursomc.recources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaRecource {
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "Rest esta funcionando!";
+	}
+
+}
